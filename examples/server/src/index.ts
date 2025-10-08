21950e7a7ceaec4caf6d12d740b677d7d485ce9d
cameronapak <cameronandrewpak@gmail.com>
2025-10-08 16:48:29 -0600

    fix(server): start hono server with node serve

--- examples/server/src/index.ts
+++ examples/server/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { appRouter } from './routers';
+import { serve } from "@hono/node-server";
 
 const app = new Hono();
 
@@ -22,10 +23,22 @@ app.get("/", (c) => {
 	return c.text("OK");
 });
 
+serve(
+	{
+		// Railway assigns a random port to expose for each deployment, 
+		// which can be accessed via the PORT environment variable.
+		port: Number(process.env.PORT) || 3000,
+		fetch: app.fetch
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`);
+	},
+);
+
 export default {
 	// Railway assigns a random port to expose for each deployment, 
 	// which can be accessed via the PORT environment variable.
-  port: Number(process.env.PORT) || 3000,
-  fetch: app.fetch
+	port: Number(process.env.PORT) || 3000,
+	fetch: app.fetch
 }
 
